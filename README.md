8c49c55e53a0cf1cb1d9cb2a6a1c3fb9733668a5
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-02-24 15:28:58 +0000

    Updates README with Docker deployment information

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 # devnews-core
 Developer news aggregation engine and API endpoints
+
+### Deploying
+
+**Docker** 
+
+```
+git clone https://github.com/imjacobclark/devnews-core.git && cd devnews-core
+docker build -t devnews-core .
+docker run -d -p 1337:1337 --name devnews-core devnews-core
+```
+
+The API will now be exposed at `http://localhost:133:`
--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
-var http    = require('http'),
-    q       = require('q'),
-    Feeds   = require('../feeds/Feeds'),
-    feeds   = new Feeds();
+var http    = require('http');
+var NodeCache      = require( "node-cache" );
+var cache          = new NodeCache({ stdTTL: 600, checkperiod: 601  });
 
-feeds.setCachedNews().then(function(data){
-    if(data == true)
-        serve();
+NodeCache.prototype.context = this; // set a content property of this for use in the EventEmitter callback below
+
+function setCache(){
+     return cache.set("data", {"some": "data"});
+}
+
+function getCache(){
+     return cache.get("data");
+}
+
+cache.on("expired", function(){
+    this.context.setCache();
 });
+
+if(setCache() == true)
+        serve();
 
 function serve(){
     http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-        res.end(JSON.stringify(feeds.getCachedNews())); 
+        res.end(JSON.stringify(getCache())); 
     }).listen(process.env.PORT || 1337, '0.0.0.0');
 };
